efff9089ed994e7ebc394e96e42691231adbddfc
jindah <>
2023-06-20 21:13:59 +0000

    Fix bug with score not saving

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -11,5 +11,5 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 highScoresList.innerHTML =
     highScores.map(score => {
         return `<li class="highScore">${score.name} - ${score.score}</li>`;
-    })join('');
+    }).join('');
 
